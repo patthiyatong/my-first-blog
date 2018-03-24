31116f499b9aa4b1e7c900112a94387f3d1d80b9
unknown <patthiya249@gmail.com>
2018-03-24 15:43:16 +0700

    fixed ALLOWED_HOST (again!)

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7ta5hhc@e2ofjnk)z8d%shy7n=1!xu=!sl@pb+eskw*l*b+-@='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '*.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
